a2848417415811123bffe995348f42a548ffcffa
zenixhuang <zenixls2@gmail.com>
2017-08-09 13:49:30 +0900

    first commit

---
+++ gspread_sync/__init__.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import AuthorizedSession
+import google_auth_httplib2
+from apiclient import discovery
+import googleapiclient
+from googleapiclient.http import build_http
+import os
+from threading import RLock
+import copy
+import cPickle
+
+discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
+
+class Authorization(object):
+    def __init__(self, path, secret_file="client_secret.json", secret_dump_path=None):
+        self.credentials = self.get_credentials(
+            path, client_secret_file=secret_file,
+            secret_dump_path=secret_dump_path)
+
+    @staticmethod
+    def get_credentials(credential_dir, client_secret_file, secret_dump_path=None,
+                        scopes=['https://www.googleapis.com/auth/spreadsheets'],
+                        redirect_uri='urn:ietf:wg:oauth:2.0:oob'):
+        if os.path.exists(secret_dump_path):
+            with open(secret_dump_path, 'rb') as f:
+                return cPickle.load(f)
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        flow = InstalledAppFlow.from_client_secret_file(
+                client_secret_file, scopes=scopes, redirect_uri=redirect_uri)
+        flow.run_local_server()
+        session = flow.authorized_session()
+        with open(secret_dump_path, 'wb') as f:
+            cPickle.dump(session.credentails, f, cPickle.HIGHEST_PROTOCOL)
+        return session.credentials
+
+class Sheet(object):
+    def __init__(self, sheetId, auth):
+        self.sheetId = sheetId
+        if isinstance(auth, Authorization):
+            self.credentials = copy.deepcopy(auth.credentials)
+        else:
+            self.credentials = copy.deepcopy(auth)
+
+        self.apiLock = RLock()
+
+        # the AuthorizedHttp already implement refresh in request
+        # so we don't have to check if is valid or not
+        http = google_auth_httplib2.AuthorizedHttp(self.credentials,
+                                                   http=build_http())
+        self.service = discovery.build(
+                'sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
+
+    def query(self, sheetName, rangeName=None, dimension='ROWS', callback=None,
+              retries=3):
+        queryRange = createQueryRange(sheetName, rangeName)
+        try:
+            with self.apiLock:
+                result = self.service.spreadsheets().values().get(
+                        spreadsheetId=self.sheetId, range=queryRange,
+                        majorDimension=dimension).execute(num_retries=retries)
+                return result.get('values', [])
+        except googleapiclient.errors.HttpError as e:
+            if callback:
+                return callback(e)
+            else:
+                raise e
+        return []
+
+    def update(self, sheetName, rangeName=None, body=[], callback=None,
+            retries=3):
+        queryRange = createQueryRange(sheetName, rangeName)
+        try:
+            with self.apiLock:
+                request = self.service.spreadsheets().values().update(
+                        spreadsheetId=self.sheetId, range=queryRange,
+                        body={"values": body}, valueInputOption="RAW").execute(num_retries=retries)
+                return request
+        except googleapiclient.errors.HttpError as e:
+            if callback:
+                return callback(e)
+            else:
+                raise e
+        return {}
+
+    def newSheet(self, sheetName, header=[], callback=None, retries=3):
+        queryRange = createQueryRange(sheetName)
+        try:
+            with self.apiLock:
+                result = self.service.spreadsheets().batchUpdate(
+                        spreadsheetId=self.sheetId,
+                        body={"results": [{
+                            "addSheet": {
+                                "properties": {
+                                    "title": sheetName,
+                                    "gridProperties": {
+                                        "columnCount": len(header),
+                                        "rowCount": 1000
+                                    }
+                                }
+                            }
+                        }]}).execute(num_retries=retries)
+                if len(header) == 0:
+                    return
+                newQueryRange = createQueryRange(
+                        sheetName, 'A1:' + int26(len(header)-1) + '1')
+                request = self.service.spreadsheets().values().update(
+                        spreadsheeetId=self.sheedId, range=newQueryRange, body={
+                            'values': [header]}, valueInputOption='RAW').execute(num_retries=retries)
+            except googleapiclient.errors.HttpError as e:
+                if callback:
+                    return callback(e)
+                else:
+                    raise e
+            return []
+
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gspread_sync',
+    version='0.0.1',
+    description='google spreadsheet wrapper with multithread safety',
+    long_description=long_description,
+    url='https://github.com/zenixls2/gspread_sync',
+    author='Zenix Huang',
+    author_email='zenixls2@gmail.com',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Monitoring',
+    ],
+    keywords='spreadsheet google',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['google-api-python-client', 'httplib2', 'google-auth',
+                      'google-auth-oauthlib', 'google-auth-httplib2'],
+)
