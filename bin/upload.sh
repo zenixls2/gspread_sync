4d69f110c9165f2900123f653865b101d3218a61
Zenix <zenixls2@gmail.com>
2019-04-23 19:06:35 +0900

    update upload script

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT=`realpath $0`
 SCRIPT_DIR=`dirname $SCRIPT`
-cd $SCRIPT_DIR/.. && python setup.py sdist bdist_wheel upload
+cd $SCRIPT_DIR/.. && python setup.py sdist bdist_wheel && twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
